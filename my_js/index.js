bbc503c287ad1a31764f153692f19e3d9abbc182
Ahmet Polat <polat2000@gmail.com>
2021-06-14 16:48:13 +0300

    expanded language support and added project imgs

--- my_js/index.js
+++ my_js/index.js
@@ -4,10 +4,23 @@ if (!lang) {
     window.localStorage.setItem("lang", "de");
 }
 
+$(function() {
+    
+});
+
 const jsonXHR = $.getJSON("./all_texts.json", function (data) {
 
 
     $("#main_header").html(data.main_header_text[lang]);
+    $("#projects_header").html(data.projects_header_text[lang]);
+    $("#collection_header").html(data.collection_header_text[lang]);
+    $("#collection_sub_header").html(data.collection_sub_header_text[lang]);
+    $("#changeLanguageButton").html(data.lang_verbose[lang]);
 });
 
 console.log(jsonXHR);
+
+function changeLang(lan) {
+    window.localStorage.setItem("lang", lan);
+    window.location.reload()
+}
