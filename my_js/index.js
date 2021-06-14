52ef239c96745c9529015396306dec1225f41cd6
Ahmet Polat <polat2000@gmail.com>
2021-06-14 19:18:01 +0300

    badge explanations

--- my_js/index.js
+++ my_js/index.js
@@ -5,7 +5,7 @@ if (!lang) {
 }
 
 $(function () {
-    $('[data-toggle="popover"]').popover()
+    
 });
 
 $('.popover-dismiss').popover({
@@ -20,11 +20,27 @@ const jsonXHR = $.getJSON("./all_texts.json", function (data) {
     $("#collection_header").html(data.collection_header_text[lang]);
     $("#collection_sub_header").html(data.collection_sub_header_text[lang]);
     $("#changeLanguageButton").html(data.lang_verbose[lang]);
-    $("#vue_img").data("content", data.vue_popover_content_text[lang]);
-    console.log($("#vue_img").data());
-});
 
-console.log(jsonXHR);
+    $("#vue_img").attr({ "data-content": data.vue_popover_content_text[lang], "data-title": data.confident[lang] });
+
+    $("#python_img").attr({ "data-content": data.vue_popover_content_text[lang], "data-title": data.confident[lang] });
+    $("#node_img").attr({ "data-content": data.node_popover_content_text[lang], "data-title": data.confident[lang] });
+    $("#mongo_img").attr({ "data-content": data.mongo_popover_content_text[lang], "data-title": data.confident[lang] });
+    $("#linux_img").attr({ "data-content": data.linux_popover_content_text[lang], "data-title": data.satisfactory[lang] });
+    $("#pandas_img").attr({ "data-content": data.pandas_popover_content_text[lang], "data-title": data.confident[lang] });
+    $("#css_img").attr({ "data-content": data.css_popover_content_text[lang], "data-title": data.satisfactory[lang] });
+    $("#django_img").attr({ "data-content": data.django_popover_content_text[lang], "data-title": data.satisfactory[lang] });
+    $("#html_img").attr({ "data-content": data.html_popover_content_text[lang], "data-title": data.confident[lang] });
+    $("#matlab_img").attr({ "data-content": data.matlab_popover_content_text[lang], "data-title": data.satisfactory[lang] });
+    $("#keras_img").attr({ "data-content": data.keras_popover_content_text[lang], "data-title": data.learning[lang] });
+    $("#tensorflow_img").attr({ "data-content": data.tensorflow_popover_content_text[lang], "data-title": data.learning[lang] });
+
+
+
+
+
+    $('[data-toggle="popover"]').popover()
+});
 
 function changeLang(lan) {
     window.localStorage.setItem("lang", lan);
