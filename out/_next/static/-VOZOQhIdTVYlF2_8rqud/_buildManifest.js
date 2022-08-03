18e0cb9669b4216d525fed91a5fe0b842e32cb76
Ahmet Polat <polat2000@gmail.com>
2022-08-03 12:34:14 +0200

    Deploy gh-pages

--- out/_next/static/-VOZOQhIdTVYlF2_8rqud/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/354-3667e02d83251d47.js","static/chunks/pages/index-c2d140542e421bb5.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
