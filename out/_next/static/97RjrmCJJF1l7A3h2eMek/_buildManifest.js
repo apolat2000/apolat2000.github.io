4159e0db28d94dcfff183d2c307d50eb5383f280
Ahmet Polat <polat2000@gmail.com>
2022-08-03 12:44:55 +0200

    Deploy gh-pages

---
+++ out/_next/static/97RjrmCJJF1l7A3h2eMek/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/191-d738abff1af1e445.js","static/chunks/pages/index-0464824b20a8caf4.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- out/_next/static/yTuzXqlnNLrOiU7xZ-_do/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/191-d738abff1af1e445.js","static/chunks/pages/index-a7ae76bb8dd8909a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
